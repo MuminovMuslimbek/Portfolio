5add08e71973cd795c8b10191d6de562925ecc5e
MuminovMuslimbek <muslimbekmuminov47@gmail.com>
2024-09-15 16:15:24 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -19,3 +19,22 @@ document.addEventListener("DOMContentLoaded", function() {
         observer.observe(card);
     });
 });
+document.getElementById('find-food-button').addEventListener('click', () => {
+    document.getElementById('loader').style.display = 'flex';
+
+    let loadingTime = 0;
+    const interval = setInterval(() => {
+        loadingTime += 1;
+        if (loadingTime >= 5) {
+            clearInterval(interval);
+            document.getElementById('loader').style.display = 'none';
+
+            document.getElementById('modal').style.display = 'flex';
+        }
+    }, 1000);
+});
+
+// Modalni yopish
+document.getElementById('close-modal').addEventListener('click', () => {
+    document.getElementById('modal').style.display = 'none';
+});
